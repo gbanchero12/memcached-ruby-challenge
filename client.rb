b47c06f14957b1c1f151af3d10c860a1836ad35f
= <gbanchero@hexa.com.uy>
2020-06-04 00:19:13 -0300

    multi-client-sender-and-reply-fix

--- client.rb
+++ client.rb
@@ -1,12 +1,51 @@
 require 'socket'
-client = TCPSocket.open('localhost', 2000)
 
-command = gets.chomp
-client.puts "#{command.to_s}"
+class Client
+  def initialize(socket)
+    @socket = socket
+    @request_object = send_request
+    @response_object = listen_response
 
+    @request_object.join # will send the request to server
+    @response_object.join # will receive response from server
+  end
 
+  def send_request
+    puts "Establish a connection..."
+    begin
+      Thread.new do
+        loop do
+          message = $stdin.gets.chomp
+          @socket.puts message
+        end
+      end
+    rescue IOError => e
+      puts e.message
+      # e.backtrace
+      @socket.close
+    end
 
-while line = client.gets # Read lines from socket
-  puts line         # and print them
+  end
+
+  def listen_response
+    begin
+      Thread.new do
+        loop do
+          response = @socket.gets.chomp
+          puts "#{response}"
+          if response.eql?'quit'
+            @socket.close
+          end
+        end
+      end
+    rescue IOError => e
+      puts e.message
+      @socket.close
+    end
+  end
 end
-client.close;
+
+
+
+socket = TCPSocket.open( "localhost", 2000 )
+Client.new( socket )
